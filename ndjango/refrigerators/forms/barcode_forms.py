ffec53f97ac21fce95c55f89c6be5e240b4dcddb
SeyeonPark <seyeon47@naver.com>
2023-04-05 08:32:45 +0900

    [add] secret_key forms

---
+++ ndjango/refrigerators/forms/barcode_forms.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+from django import forms
+# from refrigerators.models.barcode_models import *
+
+# # Form (일반 폼)
+# class PostForm(forms.Form):
+# 	title = forms.CharField()
+# 	content = forms.CharField(widget=forms.Textarea)
+#
+# # Model Form (모델 폼)
+# class PostForm(forms.ModelForm):
+# 	class Meta:
+# 		model = Post
+# 		fields = ['title', 'content']
+
